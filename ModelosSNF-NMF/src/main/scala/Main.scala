60e5d5a35bdc155934696cde781078d81b630a1f
NicolasLopez147 <nilopezn@unal.edu.co>
2023-02-21 18:51:39 -0500

    Implementacion NMF

---
+++ ModelosSNF-NMF/src/main/scala/Main.scala
@@ -0,0 +1,31 @@
+import LoadData.DataMa
+import SNF.SNF
+import breeze.linalg.DenseMatrix
+import breeze.numerics.round
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    // Cargar datos
+    val (expMatriz,methyMatriz,mirnaMatriz) = LoadData.tratarDatos(Seq("aml"))
+
+
+    // Implementar model SNF
+    val modeloSNF = new SNF(exp = expMatriz, methy = methyMatriz, mirna = mirnaMatriz)
+    val (matrizEstatusPromedio,expMatrizEstatus,methyMatrizEstatus,mirnaMatrizEstatus) = modeloSNF.aplicarSNF(m = 0.5,porcentaje = 5,iteraciones = 20)
+
+    for (elemento <- matrizEstatusPromedio.activeIterator) println(elemento)
+
+    //Implementar modelo NMF
+    /*val modelo = Trainer.jnmf(Array(expMatriz.t,methyMatriz.t,mirnaMatriz.t),r = 50)
+    println(modelo.cost(Array(expMatriz.t,methyMatriz.t,mirnaMatriz.t)))*/
+
+    /*val row = 100
+    val u = DenseMatrix.rand[Double](row, 4000)
+    val v = DenseMatrix.rand[Double](row, 2000)
+    val y = DenseMatrix.rand[Double](row, 2500)
+
+    val model = Trainer.jnmf(Array(u, v, y), r = 20)
+    println(model.cost(Array(u, v, y)))*/
+
+  }
+}
