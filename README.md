3e71ea91d2a9c90a500057f5f64cf67e0fe5d647
Danny Garzón <degarzonm@unal.edu.co>
2023-03-22 09:43:26 -0500

    JNMF is failing test

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Multi-View-learning
 
-This is an implementation of diverse machine learning algorithms to enhance the data for multi-view learning. The code is based on the implementation of [1], [2] and [3] the goal is to use these methods over cancer data, using the [Multi-omics Data Integration, Interpretation, and Its Application](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7003173/)
+This is an implementation of diverse machine learning algorithms to enhance the data for multi-view learning. The code is based on the implementation of [1], [2] and [3] the goal is to use these methods over cancer data, using the [Multi-omics Data Integration, Interpretation, and Its Application](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7003173/) paper as a reference.
+
 [1] Joint nonnegative matrix factorization for multi-view data. 
 
-[2] Similarity network fusion for aggregating data types on a genomic scale.
+[2] [Similarity network fusion for aggregating data types on a genomic scale.](http://compbio.cs.toronto.edu/SNF/SNF/Software.html)
 
-[3] Kernel K Means Clustering.
+[3] [Kernel K Means Clustering](https://sites.google.com/site/dataclusteringalgorithms/kernel-k-means-clustering-algorithm)
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,10 +5,11 @@ import breeze.linalg.{DenseMatrix, sum}
 import breeze.numerics._
 import breeze.linalg.DenseVector
 
-/**
-  * This is an example of how to use the SNF model and NMF model. To compare the results of the SNF model with the NMF model,
-  * the Kernel K-Means model is used to cluster the data. The results are compared using the metrics: Silhouette, Davies-Bouldin,
-  * PSNR, Ball-Hall, Calinski-Harabasz, Hartigan and Xu.
+/** This is an example of how to use the SNF model and NMF model. To compare the
+  * results of the SNF model with the NMF model, the Kernel K-Means model is
+  * used to cluster the data. The results are compared using the metrics:
+  * Silhouette, Davies-Bouldin, PSNR, Ball-Hall, Calinski-Harabasz, Hartigan and
+  * Xu.
   */
 object Main {
   // Number of iterations for the Kernel K-Means model
@@ -16,69 +17,76 @@ object Main {
   // Number of clusters for the Kernel K-Means model
   val k = 30
 
-  /**
-    * main method
+  /** main method
     *
     * @param args
     */
   def main(args: Array[String]): Unit = {
-    //creaate 3 random matrices to use nmf
-    val X: Array[DenseMatrix[Double]] = Array(
-    DenseMatrix.rand[Double](50, 50),
-    DenseMatrix.rand[Double](50, 50)
-  )
 
-    // Implement JNMF model
-   val modeloEntrenado = Trainer.jnmf(X,r=6, eps = 0.0001 )
-    val X_reconstructed = modeloEntrenado.inverse()
-
-    //subtract each matrix from the reconstructed matrix
-    val X1_diff = X(0) - X_reconstructed(0)
-    val X2_diff = X(1) - X_reconstructed(1)
-
-
-    // Apply JNMF
-    val suma = sum(abs(X1_diff))
-    val suma2 = sum(abs(X2_diff))
-    println("Suma: " + suma)
-    println("Suma2: " + suma2)
+    for (i <- 0 to 20) {
+      // create random matrices to test the JNMF model
+      val X_original: Array[DenseMatrix[Double]] = Array(
+        DenseMatrix.rand[Double](22, 22),
+        DenseMatrix.rand[Double](22, 22)
+      )
+      // Implement JNMF model
+      val modeloEntrenado = Trainer.jnmf(X_original, r = 6, eps = 0.0001)
+      // Print the cost of the model
+      println(modeloEntrenado.cost(X_original))
+    }
   }
 
-  def mainTemp():Unit = {
- // Load data
-    val (expMatriz,methyMatriz,mirnaMatriz) = LoadData.tratarDatos(Seq("resources"))
+  def mainTemp(): Unit = {
+    // Load data
+    val (expMatriz, methyMatriz, mirnaMatriz) =
+      LoadData.tratarDatos(Seq("resources"))
 
     // Implement SNF model
-    val modeloSNF = new SNF(exp = expMatriz, methy = methyMatriz, mirna = mirnaMatriz)
+    val modeloSNF =
+      new SNF(exp = expMatriz, methy = methyMatriz, mirna = mirnaMatriz)
 
     // Apply SNF
     val (
       matrizEstatusPromedio,
       expMatrizEstatus,
       methyMatrizEstatus,
-      mirnaMatrizEstatus,
-    ) = modeloSNF.aplicarSNF(m = 0.5,porcentaje = 5,iteraciones = 20)
+      mirnaMatrizEstatus
+    ) = modeloSNF.aplicarSNF(m = 0.5, porcentaje = 5, iteraciones = 20)
 
     // Apply Kernel K-Means
-    val est_prom_labels = KKMeans.kernel_k_means(matrizEstatusPromedio, k, iterations)
+    val est_prom_labels =
+      KKMeans.kernel_k_means(matrizEstatusPromedio, k, iterations)
 
     // Build a table with the results based on the metrics
-    val table:Seq[Seq[Any]] = Seq(
+    val table: Seq[Seq[Any]] = Seq(
       Seq("Metrica", "Kernel"),
-      Seq("Silhouette", Metrics.silhouette(matrizEstatusPromedio, est_prom_labels)),
-      Seq("Davies-Bouldin", Metrics.davies_bouldin_index(matrizEstatusPromedio, est_prom_labels)),
+      Seq(
+        "Silhouette",
+        Metrics.silhouette(matrizEstatusPromedio, est_prom_labels)
+      ),
+      Seq(
+        "Davies-Bouldin",
+        Metrics.davies_bouldin_index(matrizEstatusPromedio, est_prom_labels)
+      ),
       Seq("PSNR", Metrics.psnr(matrizEstatusPromedio, est_prom_labels)),
-      Seq("Ball-Hall", Metrics.ball_hall(matrizEstatusPromedio, est_prom_labels)),
-      Seq("Calinski-Harabasz", Metrics.calinski_harabasz(matrizEstatusPromedio, est_prom_labels)),
+      Seq(
+        "Ball-Hall",
+        Metrics.ball_hall(matrizEstatusPromedio, est_prom_labels)
+      ),
+      Seq(
+        "Calinski-Harabasz",
+        Metrics.calinski_harabasz(matrizEstatusPromedio, est_prom_labels)
+      ),
       Seq("Hartigan", Metrics.hartigan(matrizEstatusPromedio, est_prom_labels)),
-      Seq("Xu", Metrics.xu(matrizEstatusPromedio, est_prom_labels)),
+      Seq("Xu", Metrics.xu(matrizEstatusPromedio, est_prom_labels))
     )
 
     // Print the table for the SNF model
     printTable(table);
 
     // Implement NMF model
-    val NMFmodel = Trainer.jnmf(Array(expMatriz, methyMatriz, mirnaMatriz), r = k)
+    val NMFmodel =
+      Trainer.jnmf(Array(expMatriz, methyMatriz, mirnaMatriz), r = k)
 
     // Apply NMF
     val W_matrix = NMFmodel.w
@@ -87,7 +95,7 @@ object Main {
     val w_labels = KKMeans.kernel_k_means(W_matrix, W_matrix.cols, iterations)
 
     // Build a table with the results based on the metrics
-    val table2:Seq[Seq[Any]] = Seq(
+    val table2: Seq[Seq[Any]] = Seq(
       Seq("Metrica", "Kernel"),
       Seq("Silhouette", Metrics.silhouette(W_matrix, w_labels)),
       Seq("Davies-Bouldin", Metrics.davies_bouldin_index(W_matrix, w_labels)),
@@ -95,7 +103,7 @@ object Main {
       Seq("Ball-Hall", Metrics.ball_hall(W_matrix, w_labels)),
       Seq("Calinski-Harabasz", Metrics.calinski_harabasz(W_matrix, w_labels)),
       Seq("Hartigan", Metrics.hartigan(W_matrix, w_labels)),
-      Seq("Xu", Metrics.xu(W_matrix, w_labels)),
+      Seq("Xu", Metrics.xu(W_matrix, w_labels))
     )
 
     // Print the table for the NMF model
@@ -103,15 +111,19 @@ object Main {
 
   }
 
-  /**
-    * Print a table with the results of the metrics
+  /** Print a table with the results of the metrics
     *
     * @param table
     */
   def printTable[T](table: Seq[Seq[T]]): Unit = {
     val colWidths = table.transpose.map(_.map(_.toString.length).max)
     table.foreach { row =>
-      println(row.zip(colWidths).map { case (elem, width) => elem.toString.padTo(width, ' ') }.mkString(" "))
+      println(
+        row
+          .zip(colWidths)
+          .map { case (elem, width) => elem.toString.padTo(width, ' ') }
+          .mkString(" ")
+      )
     }
   }
 }
